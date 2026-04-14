d5b619282c2f886b581110c326f34c51736570b1
Hamza Yousuf GIT <roxerhamza@gmail.com>
2026-04-14 11:13:09 +0000

    01 Variables

---
+++ 01-Basics/01-variables.js
@@ -0,0 +1,10 @@
+const accountId = 221122110
+let accountEmail = "hamza2211@fake.com"
+var accountCity = "rawalpindi"
+
+/*
+    Prefer don't use var, delete it from your mind, I mean don't use it
+*/
+
+accountId = 23450
+console.table([accountEmail, accountId, accountCity])
